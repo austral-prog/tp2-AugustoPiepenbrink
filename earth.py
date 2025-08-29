8b7e0b70de1105fa7e9f2318572acf4e1636eee5
AugustoPiepenbrink <apiepenbrink@mail.austral.edu.ar>
2025-08-29 12:31:59 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,9 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+	xlower = x < y
+	xhigher = y < x
+	ms1 = "The result of {X} comes first in the dictionary than {Y} is {xlower}"
+	ms2 = "TThe result of {Y} comes first in the dictionary than {X} is {xhigher}."
+	print(ms1)
+	print(ms2)
