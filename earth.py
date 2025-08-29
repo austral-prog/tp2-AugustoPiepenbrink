1f4939d2589ebd33f5679fe21611eb0763e4f703
AugustoPiepenbrink <apiepenbrink@mail.austral.edu.ar>
2025-08-29 12:46:40 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -3,7 +3,7 @@ def earth():
     y = "Barbados"
 	xlower = x < y
 	xhigher = y < x
-	ms1 = "The result of {X} comes first in the dictionary than {Y} is {xlower}"
-	ms2 = "TThe result of {Y} comes first in the dictionary than {X} is {xhigher}."
+	ms1 = "The result of {x} comes first in the dictionary than {y} is {xlower}."
+	ms2 = "TThe result of {y} comes first in the dictionary than {x} is {xhigher}."
 	print(ms1)
 	print(ms2)
