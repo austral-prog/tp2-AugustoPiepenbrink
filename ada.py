c45ebf83eba7d52002a8bd9090477eae432e2f24
AugustoPiepenbrink <apiepenbrink@mail.austral.edu.ar>
2025-09-11 19:53:04 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,10 +1,10 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
-    r1 =  first_name.lower() + " " + last_name.lower()
-	r2 = first_name.title() + " " + last_name.title()
-	r3 = first_name.upper() + " " + last_name.upper()
-	print(r1)
-	print(r2)
-	print(r3)
-	print("\t" + r1)
+    r1 = f"{first_name.lower()} {last_name.lower()}"
+    r2 = f"{first_name.title()} {last_name.title()}"
+    r3 = f"{first_name.upper()} {last_name.upper()}"
+    print(r1)
+    print(r2)
+    print(r3)
+    print("\t" + r1)
